cea998a11c0cb6e8dcf3a667fdeb6b3262b1fd05
ewheeler <evanmwheeler@gmail.com>
2012-10-22 19:44:43 -0400

    add ISO 4217 currency info, update example output
    also add colored output, update decoding of
    iso names as they are no longer encoded as cp1252

--- get_countries_of_earth.py
+++ get_countries_of_earth.py
@@ -5,6 +5,24 @@ import codecs
 import urllib
 import argparse
 from lxml import html
+from lxml import etree
+
+# some ANSI colors, etc
+BLUE = '\033[94m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+RED = '\033[91m'
+BOLD = '\033[1m'
+ENDC = '\033[0m'
+
+def print_info(string):
+    print GREEN + string + ENDC
+
+def print_warn(string):
+    print YELLOW + string + ENDC
+
+def print_error(string):
+    print RED + string + ENDC
 
 try:
     import simplejson as json
@@ -13,55 +31,67 @@ except ImportError:
 
 def fetch_and_write(options):
     # fetch ISO short names in English and French
+    print_info('Fetching English country names and codes...')
     iso_names_en = urllib.urlretrieve('http://www.iso.org/iso/list-en1-semic-3.txt')
+    print_info('Fetching French country names and codes...')
     iso_names_fr = urllib.urlretrieve('http://www.iso.org/iso/list-fr1-semic.txt')
 
     # dict for combining en and fr names
     # {alpha2: {'short_name_en': en, 'short_name_fr': fr}}
     iso_names = {}
 
+    # dict for looking up alpha2 from short_name_en
+    en_names = {}
+
     # urllib.urlretrieve returns a tuple of (localfile, headers)
     with open(iso_names_en[0], "rU") as fin:
         for line in fin:
-            # decode iso.org's silly windows encoding
-            line = line.decode('cp1252')
-            # strip line endings, etc
-            line = line.rstrip()
-            # fields are semicolon delineated,
-            # so split into separate parts
-            if ';' in line:
-                semi = line.index(';')
-                name = line[:semi]
-                alpha2 = line[semi+1:]
-                if name and alpha2:
-                    iso_names.update({alpha2: {'short_name_en': name}})
+            try:
+                line = line.decode('utf8')
+                # strip line endings, etc
+                line = line.rstrip()
+                # fields are semicolon delineated,
+                # so split into separate parts
+                if ';' in line:
+                    semi = line.index(';')
+                    name = line[:semi]
+                    alpha2 = line[semi+1:]
+                    if name and alpha2:
+                        iso_names.update({alpha2: {'short_name_en': name}})
+                        en_names.update({name: alpha2})
+            except UnicodeDecodeError:
+                print_warn('Unable to decode ENGLISH country name: %s' % line)
 
     with open(iso_names_fr[0], "rU") as fin:
         for line in fin:
-            line = line.decode('cp1252')
-            line = line.rstrip()
-            if ';' in line:
-                semi = line.index(';')
-                name = line[:semi]
-                alpha2 = line[semi+1:]
-                if name and alpha2:
-                    if alpha2 in iso_names:
-                        # alpha2 should be in iso_names because
-                        # english was parsed first,
-                        # so append french name to list
-                        names = iso_names[alpha2]
-                        names.update({'short_name_fr': name})
-                        iso_names.update({alpha2: names})
-                    else:
-                        # hopefully this doesnt happen, but
-                        # in case there was no english name,
-                        # add french with a blank space where
-                        # english should be
-                        names = {'short_name_en': '', 'short_name_fr': name}
-                        iso_names.update({alpha2: names})
+            try:
+                line = line.decode('utf8')
+                line = line.rstrip()
+                if ';' in line:
+                    semi = line.index(';')
+                    name = line[:semi]
+                    alpha2 = line[semi+1:]
+                    if name and alpha2:
+                        if alpha2 in iso_names:
+                            # alpha2 should be in iso_names because
+                            # english was parsed first,
+                            # so append french name to list
+                            names = iso_names[alpha2]
+                            names.update({'short_name_fr': name})
+                            iso_names.update({alpha2: names})
+                        else:
+                            # hopefully this doesnt happen, but
+                            # in case there was no english name,
+                            # add french with a blank space where
+                            # english should be
+                            names = {'short_name_en': '', 'short_name_fr': name}
+                            iso_names.update({alpha2: names})
+            except UnicodeDecodeError:
+                print_warn('Unable to decode FRENCH country name: %s' % line)
 
     # fetch content of statoids.com country code page
     statoids_url = "http://www.statoids.com/wab.html"
+    print_info('Fetching other country codes...')
     content = urllib.urlopen(statoids_url).read()
     doc = html.fromstring(content)
 
@@ -70,9 +100,9 @@ def fetch_and_write(options):
     column_names = ["Entity", "ISO3166-1-Alpha-2","ISO3166-1-Alpha-3","ISO3166-1-numeric","ITU","FIPS","IOC","FIFA","DS","WMO","GAUL","MARC","Dial","is_independent"]
     alpha2_key = "ISO3166-1-Alpha-2"
 
-    """
     # comment out the preceding two lines and
     # uncomment these lines to use statoids.com column names
+    """
     column_names = []
     alpha2_key = 'A-2'
     for tr in doc.find_class('hd'):
@@ -127,10 +157,68 @@ def fetch_and_write(options):
             ckey = cinfo[keyed_by]
             country_info.update({ckey: cinfo})
 
+    # fetch iso currency codes 
+    currency_url = "http://www.currency-iso.org/dl_iso_table_a1.xml"
+    print_info('Fetching currency codes...')
+    currencies_xml_str = urllib.urlopen(currency_url).read()
+    currencies = etree.fromstring(currencies_xml_str)
+
+    currency_tag_map = {\
+            u"ENTITY": u"currency_short_name_en",
+            u"CURRENCY": u"currency_name",
+            u"ALPHABETIC_CODE": u"currency_alphabetic_code",
+            u"NUMERIC_CODE": u"currency_numeric_code",
+            u"MINOR_UNIT": u"currency_minor_unit"
+    }
+    currency_country_name_map = {
+            u"CONGO, THE DEMOCRATIC REPUBLIC OF": "CONGO, THE DEMOCRATIC REPUBLIC OF THE",
+            u"EUROPEAN UNION ": None,
+            u"HEARD ISLAND AND McDONALD ISLANDS": "HEARD ISLAND AND MCDONALD ISLANDS",
+            u"INTERNATIONAL MONETARY FUND (IMF) ": None,
+            u"KOREA, DEMOCRATIC PEOPLE’S REPUBLIC OF": "KOREA, DEMOCRATIC PEOPLE'S REPUBLIC OF",
+            u"LAO PEOPLE’S DEMOCRATIC REPUBLIC": "LAO PEOPLE'S DEMOCRATIC REPUBLIC",
+            u"MEMBER COUNTRIES OF THE AFRICAN DEVELOPMENT BANK GROUP": None,
+            u"SERBIA ": "SERBIA",
+            u"SISTEMA UNITARIO DE COMPENSACION REGIONAL DE PAGOS \"SUCRE\"": None,
+            u"Vatican City State (HOLY SEE)": "HOLY SEE (VATICAN CITY STATE)",
+            u"VIRGIN ISLANDS (BRITISH)": "VIRGIN ISLANDS, BRITISH",
+            u"VIRGIN ISLANDS (US)": "VIRGIN ISLANDS, U.S.",
+            u"ZZ01_Bond Markets Unit European_EURCO": None,
+            u"ZZ02_Bond Markets Unit European_EMU-6": None,
+            u"ZZ03_Bond Markets Unit European_EUA-9": None,
+            u"ZZ04_Bond Markets Unit European_EUA-17": None,
+            u"ZZ05_UIC-Franc": None,
+            u"ZZ06_Testing_Code": None,
+            u"ZZ07_No_Currency": None,
+            u"ZZ08_Gold": None,
+            u"ZZ09_Palladium": None,
+            u"ZZ10_Platinum": None,
+            u"ZZ11_Silver": None,
+    }
+    for iso_currency_element in currencies.iterchildren():
+        currency_dict = {}
+        for currency_tag in iso_currency_element.iterchildren():
+            currency_dict.update({currency_tag_map[currency_tag.tag]: currency_tag.text})
+        currency_alpha2 = None
+        currency_name = currency_dict['currency_short_name_en'].replace(u'\xa0',u'')
+        try:
+            currency_alpha2 = en_names[currency_name]
+        except KeyError:
+            currency_alpha2 = en_names.get(currency_country_name_map.get(currency_name))
+
+        if currency_alpha2:
+            country_info[currency_alpha2].update(currency_dict)
+        else:
+            print_warn('Failed to match currency data for country: %s' % currency_name)
+
     # dump dict as json to file
-    f = open("countries-of-earth.json", mode='w')
+    output_filename = "countries-of-earth.json"
+    if options.outfile:
+        output_filename = options.outfile
+    f = open(output_filename, mode='w')
     stream = codecs.getwriter('utf8')(f)
     json.dump(country_info, stream, ensure_ascii=False, indent=2, encoding='utf-8')
+    print_info('Saved country data to: %s' % output_filename)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Fetch current ISO 3166 country codes and other standards and output as JSON file')
