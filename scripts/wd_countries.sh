e674d70e7b55bb81ce75bfd600f836e742936950
User <ppkrauss@gmail.com>
2018-08-03 12:39:13 -0300

    script for issue #53

---
+++ scripts/wd_countries.sh
@@ -0,0 +1,17 @@
+## retrieving Wikidata dataset by SparQL:
+curl -o wd_countries.csv -G 'https://query.wikidata.org/sparql' \
+     --header "Accept: text/csv"  \
+     --data-urlencode query='
+ SELECT DISTINCT (?simple_value AS ?iso2_code) ?wd_id
+ WHERE {
+   ?item p:P297 ?statement .
+   ?statement ps:P297 ?simple_value .
+   OPTIONAL { ?statement pq:P582 ?qualifier . }
+   FILTER ( !bound(?qualifier) )
+   BIND ( strafter(str(?item), str(wd:)) AS ?wd_id ).
+ } ORDER BY ?iso2_code
+'
+# Eliminate duplication (confusion with kingdoms and territories)
+# in the future we can use "P31 Q417175" to eliminate doublets of kingdows, but "territory vs nation" need some check.
+# so, filtering invalid doublets and saving with same name:
+grep -v 'Q756617\|Q29999\|Q29999' wd_countries.csv  | sponge wd_countries.csv
